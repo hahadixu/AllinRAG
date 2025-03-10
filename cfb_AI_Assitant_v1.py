8a6cce0105183231dca367f2d9499359398a27b0
MS.xu <34087830+hahadixu@users.noreply.github.com>
2025-03-10 16:07:59 +0800

    cfb_AI_Assitant.py

--- cfb_AI_Assitant_v1.py
+++ cfb_AI_Assitant_v1.py
@@ -1,9 +1,9 @@
-# 环境变量加载保持不变
+# 环境变量
 from dotenv import load_dotenv
 
 load_dotenv()
 import os
-# 1.Load 导入Document Loaders
+# 导入Document Loaders
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.document_loaders import Docx2txtLoader
 from langchain_community.document_loaders import UnstructuredExcelLoader
@@ -12,7 +12,7 @@ from langchain_community.document_loaders import UnstructuredExcelLoader
 base_dir = './cfb' # 文档的存放目录
 documents = []
 for file in os.listdir(base_dir):
-    # 构建完整的文件路径
+    # 文件路径
     file_path = os.path.join(base_dir, file)
     if file.endswith('.pdf'):
         loader = PyPDFLoader(file_path)
@@ -24,12 +24,12 @@ for file in os.listdir(base_dir):
  #       loader = UnstructuredExcelLoader(file_path)
   #      documents.extend(loader.load())
 
-# 2.Split 将Documents切分成块以便后续进行嵌入和向量存储
+# 将Documents分块
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=10)
 chunked_documents = text_splitter.split_documents(documents)
 
-# 3.Store 将分割嵌入并存储在矢量数据库Qdrant中
+# 嵌入和存储
 from langchain_community.vectorstores import Qdrant
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
@@ -38,22 +38,21 @@ model_path = "BAAI/bge-small-zh-v1.5"
 embeddings = HuggingFaceEmbeddings(model_name=model_path)
 
 vectorstore = Qdrant.from_documents(
-    documents=chunked_documents, # 以分块的文档
+    documents=chunked_documents, # 分块的文档
     embedding=embeddings,
     location=":memory:",  # in-memory 存储
     collection_name="my_documents",) # 指定collection_name
 
-# 4. Retrieval 准备模型和Retrieval链
-import logging # 导入Logging工具
-from langchain_community.chat_models import ChatOpenAI # ChatOpenAI模型
-from langchain.retrievers.multi_query import MultiQueryRetriever # MultiQueryRetriever工具
-from langchain.chains import RetrievalQA # RetrievalQA链
+# 模型和Retrieval链
+import logging 
+from langchain_community.chat_models import ChatOpenAI 
+from langchain.retrievers.multi_query import MultiQueryRetriever 
 
 # 设置Logging
 logging.basicConfig()
 logging.getLogger('langchain.retrievers.multi_query').setLevel(logging.INFO)
 
-# 实例化一个大模型工具 - OpenAI的GPT-3.5
+# 实例化一个大模型工具
 llm = ChatOpenAI(
             model="deepseek-r1",
             openai_api_key=os.getenv("OPENAI_API_KEY"),
@@ -63,8 +62,6 @@ llm = ChatOpenAI(
 # 实例化一个MultiQueryRetriever
 retriever_from_llm = MultiQueryRetriever.from_llm(retriever=vectorstore.as_retriever(), llm=llm)
 
-# 实例化一个RetrievalQA链
-#qa_chain = RetrievalQA.from_chain_type(llm,retriever=retriever_from_llm)
 
 
 from langchain.chains import create_retrieval_chain
@@ -102,11 +99,8 @@ app = Flask(__name__)  # Flask APP
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        # 接收用户输入作为问题
+        # 用户输入
         question = request.form.get('question')
-
-        # RetrievalQA链 - 读入问题，生成答案
-        #result = qa_chain({"query": question})
         result = chain.invoke({"input": question})
         print(result)
 
